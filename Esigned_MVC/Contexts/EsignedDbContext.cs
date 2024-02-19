8676fc7fdc8c261531723391616e625c830749f3
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-02-19 21:53:49 +0400

    Finished front to back implementation

---
+++ Esigned_MVC/Contexts/EsignedDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Esigned_MVC.Contexts
+{
+    public class EsignedDbContext : DbContext
+    {
+        public EsignedDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Esigned_MVC/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Esigned_MVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Portfolio()
+        {
+            return View();
+        }
+    }
+}
